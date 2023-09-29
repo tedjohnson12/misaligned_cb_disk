c9bb39146bf073f3b4da86456bbc13521fa1416e
Ted Johnson <tedjohnson12@g.ucla.edu>
2023-09-29 13:18:47 -0700

    setup repo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='misaligned-cb-disk',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/tedjohnson12/misaligned_cb_disk',
+    author='Ted Johnson',
+    author_email='ted.johnson@unlv.edu'
+)

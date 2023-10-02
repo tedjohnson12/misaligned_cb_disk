f16d282adfdddca801a8ab73aaabaebcde4c6490
Ted Johnson <tedjohnson12@g.ucla.edu>
2023-10-02 15:45:58 -0700

    add m1 m2 properties

--- misaligned_cb_disk/params.py
+++ misaligned_cb_disk/params.py
@@ -2,28 +2,47 @@ import numpy as np
 
 G = 1
 
-def get_star_masses(Mb:float,fb:float):
+def get_star_masses(mb:float,fb:float):
     """
     Get the masses of the two stars given
     the total mass and the ratio between them.
     """
-    M2 = Mb * fb
-    M1 = Mb*(1-fb)
-    return M1, M2
+    m2 = mb * fb
+    m1 = mb*(1-fb)
+    return m1, m2
 
 class Binary:
     def __init__(
         self,
-        Mb:float,
+        mb:float,
         fb:float,
         ab:float,
         eb:float,
         Tb:float
     ):
-        self.Mb = Mb
+        self.mb = mb
         self.fb = fb
         self.ab = ab
         self.eb = eb
         self.Tb = Tb
+    @property
+    def m1(self)->float:
+        """
+        The mass of Star 1
+
+        :type: float
+        """
+        m1, _ = get_star_masses(self.mb,self.fb)
+        return m1
+    @property
+    def m2(self)->float:
+        """
+        The mass of Star 2
+
+        :type: float
+        """
+        _, m2 = get_star_masses(self.mb,self.fb)
+        return m2
+    
     
     
--- test/test_params.py
+++ test/test_params.py
@@ -13,8 +13,20 @@ def test_star_mass():
         (0,1,0,0),
         (0,0,0,0)
     )
-    for (Mb,fb,M1,M2) in cases:
-        _M1, _M2 = params.get_star_masses(Mb,fb)
-        assert _M1 == pytest.approx(M1,abs=1e-6), 'Failed for M1'
-        assert _M2 == pytest.approx(M2,abs=1e-6), 'Failed for M2'
-        
+    for (mb,fb,m1,m2) in cases:
+        _m1, _m2 = params.get_star_masses(mb,fb)
+        assert _m1 == pytest.approx(m1,abs=1e-6), 'Failed for m1'
+        assert _m2 == pytest.approx(m2,abs=1e-6), 'Failed for m2'
+
+def test_Binary_star_masses():
+    cases = (
+        (1,1,0,1),
+        (1,0,1,0),
+        (2,0.5,1,1),
+        (0,1,0,0),
+        (0,0,0,0)
+    )
+    for (mb,fb,m1,m2) in cases:
+        binary = params.Binary(mb,fb,0,0,0)
+        assert binary.m1 == pytest.approx(m1,abs=1e-6), 'Failed for m1'
+        assert binary.m2 == pytest.approx(m2,abs=1e-6), 'Failed for m2'

155af42ae05f6f654e9092116a6514e1b9c092e5
Ted Johnson <ted.johnson@unlv.edu>
2023-10-06 20:33:53 -0700

    format file

--- test/test_params.py
+++ test/test_params.py
@@ -10,43 +10,47 @@ import pytest
 
 from misaligned_cb_disk import params
 
+
 def test_star_mass():
     cases = (
-        (1,1,0,1),
-        (1,0,1,0),
-        (2,0.5,1,1),
-        (0,1,0,0),
-        (0,0,0,0)
+        (1, 1, 0, 1),
+        (1, 0, 1, 0),
+        (2, 0.5, 1, 1),
+        (0, 1, 0, 0),
+        (0, 0, 0, 0)
     )
-    for (mb,fb,m1,m2) in cases:
-        _m1, _m2 = params.get_star_masses(mb,fb)
-        assert _m1 == pytest.approx(m1,abs=1e-6), 'Failed for m1'
-        assert _m2 == pytest.approx(m2,abs=1e-6), 'Failed for m2'
+    for (mb, fb, m1, m2) in cases:
+        _m1, _m2 = params.get_star_masses(mb, fb)
+        assert _m1 == pytest.approx(m1, abs=1e-6), 'Failed for m1'
+        assert _m2 == pytest.approx(m2, abs=1e-6), 'Failed for m2'
+
 
 def test_Binary_star_masses():
     cases = (
-        (1,1,0,1),
-        (1,0,1,0),
-        (2,0.5,1,1),
-        (0,1,0,0),
-        (0,0,0,0)
+        (1, 1, 0, 1),
+        (1, 0, 1, 0),
+        (2, 0.5, 1, 1),
+        (0, 1, 0, 0),
+        (0, 0, 0, 0)
     )
-    for (mb,fb,m1,m2) in cases:
-        binary = params.Binary(mb,fb,0,0)
-        assert binary.mass1 == pytest.approx(m1,abs=1e-6), 'Failed for m1'
-        assert binary.mass2 == pytest.approx(m2,abs=1e-6), 'Failed for m2'
+    for (mb, fb, m1, m2) in cases:
+        binary = params.Binary(mb, fb, 0, 0)
+        assert binary.mass1 == pytest.approx(m1, abs=1e-6), 'Failed for m1'
+        assert binary.mass2 == pytest.approx(m2, abs=1e-6), 'Failed for m2'
+
 
 def test_Binary_add_to_sim():
-    binary = params.Binary(2,0.5,1,0)
+    binary = params.Binary(2, 0.5, 1, 0)
     sim = Simulation()
     binary.add_to_sim(sim)
     primary = sim.particles['m1']
     secondary = sim.particles['m2']
     assert np.sqrt(
         (secondary.x-primary.x)**2
-        +(secondary.y-primary.y)**2
-        +(secondary.z-primary.z)**2
-    ) == pytest.approx(1,abs=1e-6), 'Failed for m2 distance from m1'
+        + (secondary.y-primary.y)**2
+        + (secondary.z-primary.z)**2
+    ) == pytest.approx(1, abs=1e-6), 'Failed for m2 distance from m1'
+
 
 def test_Binary_sim_orbit():
     outdir = Path(__file__).parent / 'output'
@@ -54,26 +58,27 @@ def test_Binary_sim_orbit():
     if not outdir.exists():
         outdir.mkdir()
 
-    binary = params.Binary(2,0.5,1,0)
+    binary = params.Binary(2, 0.5, 1, 0)
     sim = Simulation()
     binary.add_to_sim(sim)
-    
-    OrbitPlot(sim,unitlabel='[AU]').fig.savefig(outfile,facecolor='w')
+
+    OrbitPlot(sim, unitlabel='[AU]').fig.savefig(outfile, facecolor='w')
+
 
 def test_Planet_add_to_sim():
-    binary = params.Binary(2,0.6,1,0)
+    binary = params.Binary(2, 0.6, 1, 0)
     sim = Simulation()
     binary.add_to_sim(sim)
-    planet = params.Planet(0,3,0,0,0,0,0)
+    planet = params.Planet(0, 3, 0, 0, 0, 0, 0)
     planet.add_to_sim(sim)
     pl = sim.particles['p']
-    
+
     dist_from_com = np.sqrt(pl.x**2 + pl.y**2 + pl.z**2)
-    assert dist_from_com == pytest.approx(3.0,abs=1e-6)
-    
+    assert dist_from_com == pytest.approx(3.0, abs=1e-6)
+
     total_time = 6*np.pi
     nsteps = 100
-    time = np.linspace(0,total_time,nsteps)
+    time = np.linspace(0, total_time, nsteps)
     x1 = []
     x2 = []
     x3 = []
@@ -95,13 +100,12 @@ def test_Planet_add_to_sim():
     outfile = outdir / 'planet_orbit.png'
     if not outdir.exists():
         outdir.mkdir()
-    fig, ax = plt.subplots(1,1)
-    ax.plot(x1,y1)
-    ax.plot(x2,y2)
-    ax.plot(x3,y3)
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(x1, y1)
+    ax.plot(x2, y2)
+    ax.plot(x3, y3)
     ax.set_aspect('equal')
-    fig.savefig(outfile,facecolor='w')
-        
+    fig.savefig(outfile, facecolor='w')
 
 
 if __name__ in '__main__':
